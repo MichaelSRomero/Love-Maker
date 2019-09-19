13b775c70553442d3c1628058fe5a73281329f27
MichaelRomero <liikemike@aim.com>
2019-09-19 17:39:46 -0400

    added a RandomArray class and fetching to an API

---
+++ client/models/RandomArray.js
@@ -0,0 +1,39 @@
+class RandomArray {
+  constructor(array) {
+    this.array = array;
+    this.newArray = [...array];
+  }
+
+  randomize() {
+    return new Promise((resolve, reject) => {
+      if (this.array.length > 1) {
+
+        fetch(`https://words.bighugelabs.com/api/2/51bf5e6f12d9256669d1020cc4d87cfd/${this.array[1]}/json`)
+          .then((response) => {
+            return response.json();
+          })
+          .then((myJson) => {
+            const synArray = myJson.adjective.syn;
+            if (synArray) {
+              console.log("SUCCESS: ", synArray);
+              
+              const randomIndex = Math.floor(Math.random() * synArray.length);
+              this.newArray[1] = synArray[randomIndex]
+            }
+            resolve(this.newArray)
+          })
+          .catch(error => {
+            // Print out the error if any
+            console.log(`ERROR FOR: ${this.newArray[1]}`, error);
+            
+            this.newArray[1] = this.array[1];
+            resolve(this.newArray)
+          });
+      } else {
+        resolve(this.array);
+      }
+      
+    })
+  }
+
+}
